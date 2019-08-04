341d98bab38b63cd1aacf5fa0a2cdb0efb44a558
TS <1414012437@qq.com>
2019-08-04 09:12:41 +0800

    .path变化，侧边栏样式变化

--- src/router/index.js
+++ src/router/index.js
@@ -20,6 +20,7 @@ let router = new VueRouter({
       component: home,
       children: [
         { path: '', name: welcome, component: welcome },
+        { path: 'welcome', component: welcome },
         { path: 'article', component: article },
         { path: 'image', component: image },
         { path: 'publish', component: publish },
--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -5,37 +5,38 @@
       <el-menu
         :collapse="isCollapse"
         :collapse-transition="false"
-        default-active="1"
+        :default-active="$route.path"
         background-color="#002033"
         text-color="#fff"
         active-text-color="#ffd04b"
         class="my_menu"
+        router
       >
-        <el-menu-item index="1">
+        <el-menu-item index="/">
           <i class="el-icon-s-home"></i>
           <span slot="title">主页</span>
         </el-menu-item>
-        <el-menu-item index="2">
+        <el-menu-item index="/article">
           <i class="el-icon-document"></i>
           <span slot="title">内容管理</span>
         </el-menu-item>
-        <el-menu-item index="3">
+        <el-menu-item index="/image">
           <i class="el-icon-picture"></i>
           <span slot="title">素材管理</span>
         </el-menu-item>
-        <el-menu-item index="4">
+        <el-menu-item index="/publish">
           <i class="el-icon-s-promotion"></i>
           <span slot="title">发布文章</span>
         </el-menu-item>
-        <el-menu-item index="5">
+        <el-menu-item index="/comment">
           <i class="el-icon-chat-dot-round"></i>
           <span slot="title">评论管理</span>
         </el-menu-item>
-        <el-menu-item index="6">
+        <el-menu-item index="/fans">
           <i class="el-icon-present"></i>
           <span slot="title">粉丝管理</span>
         </el-menu-item>
-        <el-menu-item index="7">
+        <el-menu-item index="/setting">
           <i class="el-icon-setting"></i>
           <span slot="title">个人设置</span>
         </el-menu-item>
@@ -52,7 +53,9 @@
             <i class="el-icon-arrow-down el-icon--right"></i>
           </span>
           <el-dropdown-menu slot="dropdown">
-            <el-dropdown-item>个人设置</el-dropdown-item>
+            <el-dropdown-item>
+              <span @click="setting">个人设置</span>
+            </el-dropdown-item>
             <el-dropdown-item>退出登录</el-dropdown-item>
           </el-dropdown-menu>
         </el-dropdown>
@@ -74,7 +77,28 @@ export default {
   methods: {
     toggle () {
       this.isCollapse = !this.isCollapse
+    },
+    setting () {
+      this.$router.push('/setting')
     }
+    // home () {
+    //   this.$router.push('/')
+    // },
+    // article () {
+    //   this.$router.push('article')
+    // },
+    // image () {
+    //   this.$router.push('image')
+    // },
+    // publish () {
+    //   this.$router.push('publish')
+    // },
+    // comment () {
+    //   this.$router.push('comment')
+    // },
+    // fans () {
+    //   this.$router.push('fans')
+    // },
   }
 }
 </script>
